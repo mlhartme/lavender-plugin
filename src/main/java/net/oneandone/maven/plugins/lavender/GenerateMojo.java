bc7d076c0fbcfc52c200039f34341c7c24ad39a4
Michael Hartmeier <mlhartme.users.noreply@github.com>
2021-05-04 09:58:17 +0200

    tweaks

--- src/main/java/net/oneandone/maven/plugins/lavender/GenerateMojo.java
+++ src/main/java/net/oneandone/maven/plugins/lavender/GenerateMojo.java
@@ -18,6 +18,7 @@ package net.oneandone.maven.plugins.lavender;
 import net.oneandone.sushi.fs.Node;
 import net.oneandone.sushi.fs.World;
 import net.oneandone.sushi.fs.file.FileNode;
+import org.apache.maven.model.Scm;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
@@ -28,20 +29,29 @@ import org.apache.maven.project.MavenProject;
 
 import java.io.IOException;
 
-/**
- * Generates an application file. Merges dependency jars into a single file, prepended with a launch shell script.
- */
-@Mojo(name = "generate", defaultPhase = LifecyclePhase.PACKAGE, requiresDependencyResolution = ResolutionScope.RUNTIME,
-      threadSafe = true)
+/** Generates Lavender Properties */
+@Mojo(name = "generate", defaultPhase = LifecyclePhase.PREPARE_PACKAGE, requiresDependencyResolution = ResolutionScope.RUNTIME, threadSafe = true)
 public class GenerateMojo extends AbstractMojo {
-    protected final World world;
+    private final World world;
 
     /**
      * Directory where to place the Launch Script and the executable Jar file.
      * Usually, there's no need to change the default value, which is target.
      */
-    @Parameter(defaultValue = "${project.build.directory}")
-    protected FileNode dir;
+    @Parameter(defaultValue = "${project.build.directory}/${project.artifactId}/WEB-INF/lavender-next.properties")
+    private FileNode dest;
+
+    @Parameter(name = "name")
+    private String name;
+
+    @Parameter(name = "include")
+    private String includes;
+
+    @Parameter(name = "excludes")
+    private String excludes;
+
+    @Parameter(name = "resourcePathPrefix")
+    private String resourcePathPrefix;
 
     @Parameter(property = "project", required = true, readonly = true)
     private MavenProject project;
@@ -59,14 +69,25 @@ public class GenerateMojo extends AbstractMojo {
     }
 
     public void doExecute() throws IOException {
-        getLog().info("TODO");
+        Scm scm;
+
+        scm = project.getScm();
+
+        dest.writeLines(
+                "scm." + name + "=" + scm.getConnection() + "\n",
+                "scm." + name + ".devel=" + scm.getDeveloperConnection() + "\n",
+                "scm." + name + ".path=src/main/webapp\n",
+                "scm." + name + ".tag=" + scm.getTag() + "\n",
+                "scm." + name + ".includes=" + includes + "\n",
+                "scm." + name + ".excludes=" + excludes + "\n",
+                "scm." + name + ".resourcePathPrefix" + resourcePathPrefix + "\n");
     }
 
-    public void setDir(String dir) {
-        this.dir = world.file(dir);
+    public void setDest(String dest) {
+        this.dest = world.file(dest);
     }
 
-    public Node getDir() {
-        return dir;
+    public Node getDest() {
+        return dest;
     }
 }
